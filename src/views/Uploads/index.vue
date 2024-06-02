77cf551f4f422b2414fae5104e6466fed26abd41
OvO <143251569+OvO321@users.noreply.github.com>
2024-06-02 13:40:55 +0800

    Update index.vue

--- src/views/Uploads/index.vue
+++ src/views/Uploads/index.vue
@@ -1,12 +1,14 @@
 <script setup>
-import {useRouter} from "vue-router";
-import {useUserStore} from "@/stores/user.js";
-import {computed, onBeforeMount, ref} from "vue";
-import {Back, Plus} from '@element-plus/icons-vue'
-import {ElMessage} from "element-plus";
+//路由
+import { useRouter } from "vue-router";
+//用户信息
+import { useUserStore } from "@/stores/user.js";
+import { computed, onBeforeMount, ref } from "vue";
+import { Back, Plus } from '@element-plus/icons-vue'
+import { ElMessage } from "element-plus";
 import CardDetail from "@/components/cardDetail.vue";
-import {getCurrentTime} from "@/utils/getTime";
-import {uploadPost} from "@/apis/main";
+import { getCurrentTime } from "@/utils/getTime";
+import { uploadPost } from "@/apis/main";
 
 const router = useRouter()
 const userStore = useUserStore()
@@ -27,21 +29,25 @@ const dialogVisible = ref(false)
 const postData = ref({})
 const Post = ref({})
 const PostId = ref(0)
-
+//处理图片预览
 const handlePictureCardPreview = (uploadFile) => {
   dialogImageUrl.value = uploadFile.url
   dialogVisible.value = true
   return true
 }
+//处理图像上传失败情况
 const onError = async (error) => {
   ElMessage({
     type: 'warning',
     message: '图片上传失败'
   })
+  //用户信息
   const userStore = useUserStore();
   await userStore.userLogout()
+  //切换路由
   await router.replace('/')
 }
+//处理图片类型和图片大小
 const handleChange = (uploadFile, uploadFiles) => {
   const allowedTypes = ['image/jpeg', 'image/png', 'image/gif']; // 可接受的图片类型
   const maxSize = 2; // 最大文件大小，单位：MB
@@ -58,6 +64,7 @@ const handleChange = (uploadFile, uploadFiles) => {
   return true;
 }
 const upload = ref(null)
+//获取发布者的ID信息
 const beforeUpload = (rawFile) => {
   Post.value = {
     id: PostId.value
@@ -66,13 +73,13 @@ const beforeUpload = (rawFile) => {
 const doUploads = async () => {
   if (fileListUrl.value.length === 0) {
     ElMessage.warning(
-        '请至少上传一张图片!'
+      '请至少上传一张图片!'
     )
     return
   }
   if (title.value === '') {
     ElMessage.warning(
-        '请输入标题'
+      '请输入标题'
     )
     return
   }
@@ -81,18 +88,20 @@ const doUploads = async () => {
     content: content.value,
     user_id: userStore.userInfo.id,
   }
+
   const res = await uploadPost(data)
   PostId.value = res.info
   upload.value.submit()
-  ElMessage({type: 'success', message: '发布成功，3秒后跳转到主页'})
+  ElMessage({ type: 'success', message: '发布成功，3秒后跳转到主页' })
   setTimeout(() => {
     router.replace('/')
   }, 3000)
 
 }
+//处理图片数量
 const handleExceed = () => {
   ElMessage.warning(
-      '最多可以添加9张图片哦!'
+    '最多可以添加9张图片哦!'
   )
 }
 // 制作预览页面
@@ -103,13 +112,13 @@ const close = () => {
 const MakePrev = () => {
   if (fileListUrl.value.length === 0) {
     ElMessage.warning(
-        '请至少上传一张图片!'
+      '请至少上传一张图片!'
     )
     return
   }
   if (title.value === '') {
     ElMessage.warning(
-        '请输入标题'
+      '请输入标题'
     )
     return
   }
@@ -123,126 +132,224 @@ const MakePrev = () => {
   }
   show.value = true
 }
+
 const empty = []
+const valueTopic = ref('')
+const valueUser = ref('')
+const valueEmoji = ref('')
+const topics = [
+  {
+    value: "study",
+    label: "学习"
+  },
+  {
+    value: "selectCourse",
+    label: "选课"
+  },
+  {
+    value: "carpool",
+    label: "拼车"
+  },
+  {
+    value: "internship",
+    label: "实习"
+  },
+  {
+    value: "makeFriends",
+    label: "交友"
+  },
+]
+//获取用户信息
+
+
+const user = [
+  {
+    value: "user1",
+    label: "用户1"
+  },
+  {
+    value: "user2",
+    label: "用户2"
+  }
+]
+
+const emoji = [
+  {
+    value: "😀",
+    label: "😀 开心"
+  },
+  {
+    value: "🤣",
+    label: "🤣 笑死了"
+  },
+  {
+    value: "😂",
+    label: "😂 笑哭了"
+  },
+  {
+    value: "😁",
+    label: "😁 嘻嘻"
+  },
+  {
+    value: "😍",
+    label: "😍 花痴"
+  },
+  {
+    value: "😘",
+    label: "😘 飞吻"
+  },
+  {
+    value: "😒",
+    label: "😒 不高兴"
+  },
+  {
+    value: "😎",
+    label: "😎  墨镜笑脸"
+  },
+]
+
 </script>
 
 <template>
-  <el-row :gutter="50">
-    <el-col :span="500">
-      <div class="leftArea">
-        <h1 style="text-align: center">上传图片</h1>
+  <div>
+    <div class="box">
+      <h1 style="text-align: left;margin-left:20px;font-size:20px">发布图文</h1>
+      <div class="topArea">
+        <div style="font-size: large;">图片编辑</div>
         <div class="img-container">
-          <el-upload
-              v-model:file-list="fileList"
-              action="http://localhost:8000/upload/"
-              class="preview"
-              ref="upload"
-              list-type="picture-card"
-              multiple
-              :headers="userStore.headersObj"
-              :limit="9"
-              :on-preview="handlePictureCardPreview"
-              :on-change="handleChange"
-              :auto-upload="false"
-              :on-exceed="handleExceed"
-              :data="Post"
-              :before-upload="beforeUpload"
-              :on-error="onError"
-          >
+          <el-upload v-model:file-list="fileList" action="http://localhost:8000/upload/" class="preview" ref="upload"
+            list-type="picture-card" multiple :headers="userStore.headersObj" :limit="9"
+            :on-preview="handlePictureCardPreview" :on-change="handleChange" :auto-upload="false"
+            :on-exceed="handleExceed" :data="Post" :before-upload="beforeUpload" :on-error="onError">
             <el-icon>
-              <Plus/>
+              <Plus />
             </el-icon>
           </el-upload>
         </div>
       </div>
-    </el-col>
-    <el-col :span="500">
-      <div class="rightArea">
-        <h1 style="text-align: center">内容区</h1>
+      <div class="bottomArea">
         <div class="content-container">
-          <el-input
-              v-model="title"
-              maxlength="20"
-              placeholder="请输入标题"
-              show-word-limit
-              type="text"
-              style="margin-top: 20px;width: 80%;margin-left: 50px;"
-          />
-          <div style="margin: 50px 0"/>
-          <el-input
-              v-model="content"
-              maxlength="3000"
-              placeholder="请输入内容"
-              show-word-limit
-              type="textarea"
-              style="width: 80%;margin-left: 50px;"
-              autosize
-          />
+          <div style="margin-left: 19px;font-family: STXihei">标题</div>
+          <el-input v-model="title" maxlength="20" placeholder="填写标题，可能会有更多赞哦~" show-word-limit type="text"
+            style="margin-top: 10px;width: 80%;margin-left: 20px;" />
+          <div style="margin: 20px 0" />
+          <div style="margin-left: 19px">内容</div>
+          <el-input v-model="content" maxlength="300" placeholder="填写更全面的描述信息，让更多人看到你吧！" show-word-limit type="textarea"
+            :rows=4 style="width: 80%;margin-left: 20px; margin-top: 10px; " />
         </div>
       </div>
-    </el-col>
-    <el-col :span="50">
-      <el-button color="#2f779d" style="margin-top: 150px;color:white;" round  size="large" @click="doUploads">发布推文
+      <div class="extra-info">
+        <el-select v-model="valueTopic" placeholder="#话题" style="width: 100px; height: 30px;margin-right: 20px;">
+          <el-option v-for="item in topics" :key="item.value" :label="item.label" :value="item.value"></el-option>
+        </el-select>
+        <el-select v-model="valueUser" placeholder="@用户" style="width: 100px; height: 30px;margin-right: 20px;">
+          <el-option v-for="item in user" :key="item.value" :label="item.label" :value="item.value"></el-option>
+        </el-select>
+
+        <el-select v-model="valueEmoji" placeholder="😊表情" style="width: 100px; height: 30px;margin-right: 20px;">
+          <el-option v-for="item in emoji" :key="item.value" :label="item.label" :value="item.value"></el-option>
+        </el-select>
+      </div>
+
+      <el-button style="margin-top: 20px;margin-left: 45px; color:white;" round color="#fd5656" size="large"
+        @click="doUploads">发布推文
       </el-button>
-      <br>
-      <el-button color="#8db4ca" style="margin-top: 200px;color:white;" round type="primary" size="large" @click="MakePrev">生成预览</el-button>
-    </el-col>
-  </el-row>
+      <el-button style="margin-top: 20px; margin-left: 30px" round type="primary" size="large"
+        @click="MakePrev">生成预览</el-button>
+      <el-dialog v-model="dialogVisible">
+        <img :src="dialogImageUrl" alt="Preview Image" />
+      </el-dialog>
 
-
-  <el-dialog v-model="dialogVisible">
-    <img :src="dialogImageUrl" alt="Preview Image"/>
-  </el-dialog>
-
-  <div class="overlay" v-if="show">
-    <button class="backPage" @click="close">
-      <el-icon>
-        <Back/>
-      </el-icon>
-    </button>
-    <card-detail :detail="postData" :comments="empty" :review="true"/>
+      <div class="overlay" v-if="show">
+        <button class="backPage" @click="close">
+          <el-icon>
+            <Back />
+          </el-icon>
+        </button>
+        <card-detail :detail="postData" :comments="empty" :review="true" />
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.leftArea {
-  margin-left: 50px;
-  width: 515px;
+/* 背景框图 */
+.box {
+  height: 600px;
+  width: 600px;
+  margin: auto;
+  /* display: flex; */
+  /* flex-direction: column; */
+  border-radius: 20px;
+  border: #2c3e50 1px solid;
 }
 
-.rightArea {
-  margin-left: 50px;
-  width: 515px;
+.topArea {
+  display: block;
+  margin: auto;
+  width: 550px;
+  height: 150px;
+  font-family:
+    /* background-color: rebeccapurple; */
 }
 
 .img-container {
-  border-radius: 20px;
-  border: #2c3e50 1px solid;
-  width: 515px;
-  height: 500px;
+  display: block;
+  margin: auto;
+  margin-left: 20px;
+  margin-top: 10px;
+  width: 550px;
+  height: 150px;
   overflow: scroll;
+}
+
+/* 需要加穿透效果 */
+>>>.el-upload--picture-card,
+>>>.el-upload-list__item {
+  width: 100px;
+  height: 100px;
+  line-height: 110px;
+}
+
+
+.bottomArea {
+  display: block;
+  margin: auto;
+  width: 550px;
+
+
 }
 
 .content-container {
-  border-radius: 20px;
-  border: #2c3e50 1px solid;
-  width: 515px;
-  height: 500px;
+  margin-top: 20px;
+  height: 230px;
   overflow: scroll;
 }
 
+.extra-info {
+  margin-top: 8px;
+  margin-left: 45px;
+}
+
+.extra-info el-button {
+  width: 30px
+}
+
 .preview {
-  margin: 22px;
+  margin: 0, auto;
 }
 
 .overlay {
   position: fixed;
+  margin: auto;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: white; /* 设置透明度的背景色 */
-  z-index: 9999; /* 设置一个较大的z-index值，确保图层位于其他内容之上 */
+  background-color: white;
+  /* 设置透明度的背景色 */
+  z-index: 9999;
+  /* 设置一个较大的z-index值，确保图层位于其他内容之上 */
 }
 
 .backPage {
