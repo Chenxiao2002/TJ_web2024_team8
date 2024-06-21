151c4022fabe110feb4f9fc7293c0c9c613979e2
OvO <143251569+OvO321@users.noreply.github.com>
2024-06-21 21:06:57 +0800

    Update index.vue

--- src/views/Uploads/index.vue
+++ src/views/Uploads/index.vue
@@ -211,7 +211,7 @@ const emoji = [
 </script>
 
 <template>
-  <div>
+  <div style="height: 1200px;">
     <div class="box">
       <h1 style="text-align: left;margin-left:20px;font-size:20px">发布图文</h1>
       <div class="topArea">
