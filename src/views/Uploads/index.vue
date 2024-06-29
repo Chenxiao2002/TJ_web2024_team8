1a6cdd129e5574bec31a5ac17cb62dc6cbb53156
OvO <143251569+OvO321@users.noreply.github.com>
2024-06-29 23:25:21 +0800

    Update index.vue

--- src/views/Uploads/index.vue
+++ src/views/Uploads/index.vue
@@ -9,9 +9,17 @@ import { ElMessage } from "element-plus";
 import CardDetail from "@/components/cardDetail.vue";
 import { getCurrentTime } from "@/utils/getTime";
 import { uploadPost } from "@/apis/main";
+import { getAllId, setAdmin, block, unblock } from "@/apis/main";
+import {
+  getFocusInfo
+} from "@/apis/main";
 
 const router = useRouter()
 const userStore = useUserStore()
+
+
+
+
 const checkLogin = () => {
   if (!userStore.userInfo.id) {
     router.replace('/login')
@@ -164,19 +172,16 @@ const topics = [
     label: "交友"
   },
 ]
+const followInfo = ref([]);
 //获取用户信息
+const fetchFollowers = async () => {
+  console.log(userStore.userInfo.id);
+  const post = await getFocusInfo({ id: userStore.userInfo.id })
+  followInfo.value = post.info
+  console.log(followInfo.value);
+}
+fetchFollowers();
 
-
-const user = [
-  {
-    value: "用户1",
-    label: "wer"
-  },
-  {
-    value: "用户2",
-    label: "er"
-  }
-]
 
 const emoji = [
   {
@@ -248,7 +253,7 @@ const afterDoComment = (comment) => Details.afterDoComment(comment);
           <el-option v-for="item in topics" :key="item.value" :label="item.label" :value="item.value"></el-option>
         </el-select>
         <el-select v-model="valueUser" placeholder="@用户" style="width: 100px; height: 30px;margin-right: 20px;">
-          <el-option v-for="item in user" :key="item.value" :label="item.label" :value="item.value"></el-option>
+          <el-option v-for="item in followInfo" :key="item.value" :label="item.label" :value="item.value"></el-option>
         </el-select>
 
         <el-select v-model="valueEmoji" placeholder="😊表情" style="width: 100px; height: 30px;margin-right: 20px;">
